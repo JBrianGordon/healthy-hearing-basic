58ca530a632bfb83ee73f5ac8f149248b844231b
Bill Talkington <btalkington@healthyhearing.com>
2022-04-15 19:50:04 +0000

    Update responsive header

--- templates/plugin/BootstrapUI/layout/default.php
+++ templates/plugin/BootstrapUI/layout/default.php
@@ -25,7 +25,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css('BootstrapUI.bootstrap.min'); ?>
+    <?= $this->Html->css('responsive'); ?>
     <?= $this->Html->css(['BootstrapUI./font/bootstrap-icons', 'BootstrapUI./font/bootstrap-icon-sizes']); ?>
     <?= $this->Html->script(['BootstrapUI.popper.min', 'BootstrapUI.bootstrap.min']); ?>
 
@@ -34,11 +34,30 @@
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light sticky-top">
-      <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid mx-5">
         <a class="navbar-brand" href="#">
           <img src="/img/hh-logo.svg" alt="" width="198" height="40">
         </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenuToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenuToggler">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link text-uppercase" href="#">Find a clinic</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-uppercase" href="#">Hearing loss help</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-uppercase" href="#">Hearing aids help</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-uppercase" href="#">News</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
     <main class="main">
