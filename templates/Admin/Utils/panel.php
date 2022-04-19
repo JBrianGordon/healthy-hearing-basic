c18d9dbe72ba247240cf099fcc51ebab05b44484
Bill Talkington <btalkington@healthyhearing.com>
2022-04-19 17:10:30 +0000

    More styling for admin panel

--- templates/Admin/Utils/panel.php
+++ templates/Admin/Utils/panel.php
@@ -7,29 +7,34 @@
   <div class="row row-cols-1 g-4">
     <div class="col">
       <div class="card">
-        <h5 class="card-header">Admin</h5>
+        <h5 class="card-header bg-primary text-white">Admin</h5>
         <div class="card-body">
           <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-2">
-            <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 1</button>
+            <div class="col dropdown">
+              <button class="btn btn-outline-secondary dropdown-toggle w-100 rounded-pill" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Button text long 1</button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 2</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 2</button>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 3</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 3</button>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 4</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 4</button>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 5</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 5</button>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 6</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 6</button>
             </div>
             <div class="col">
-              <button class="btn btn-primary w-100 rounded-pill" type="button">Button text long 7</button>
+              <button class="btn btn-outline-secondary w-100 rounded-pill" type="button">Button text long 7</button>
             </div>
         </div>
         </div>
