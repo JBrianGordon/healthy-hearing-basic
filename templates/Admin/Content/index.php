181b91d28d6575bbf012350ee3cb858357eb4f05
Bill Talkington <btalkington@healthyhearing.com>
2022-04-29 15:37:54 +0000

    Admin Content panel changes: use floating labels and make collapsible

--- templates/Admin/Content/index.php
+++ templates/Admin/Content/index.php
@@ -4,61 +4,98 @@
  * @var \App\Model\Entity\Content[]|\Cake\Collection\CollectionInterface $content
  */
 ?>
+
 <div class="content index">
     <?= $this->Html->link(__('New Content'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Content') ?></h3>
+    <div class="d-grid justify-content-end">
+        <button class="btn btn-primary mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#advancedSearch" aria-expanded="false" aria-controls="advancedSearch">
+            + Advanced
+        </button>
+    </div>
 
-    <?php
-        echo $this->Form->create(null, ['valueSources' => 'query']);
-        echo $this->Form->control('id', ['label' => 'Content ID']);
-        echo $this->Form->control('user_id', ['label' => 'Primary Author', 'empty' => '(select one)']);
-        echo $this->Form->control('type', ['type' => 'select', 'options' => $typeOptions, 'empty' => '(select one)']);
-        echo $this->Form->control('title');
-        echo $this->Form->control('short');
-        echo $this->Form->control('body');
-        echo $this->Form->control('is_active', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+    <div class="collapse" id="advancedSearch">
+        <?php
+            echo $this->Form->create(null, [
+                'class' => 'bg-light mb-3 p-5',
+                'valueSources' => 'query'
+            ]);
+            echo $this->Form->control('id', [
+                'label' => [
+                    'text' => 'Content ID',
+                    'floating' => true
+                ]
+            ]);
+            echo $this->Form->control('user_id', [
+                'label' => [
+                    'text' => 'Primary Author',
+                    'floating' => true
+                ],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('is_library_item', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+            echo $this->Form->control('type', [
+                'type' => 'select',
+                'options' => $typeOptions,
+                'label' => ['floating' => true],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('library_share_text');
-        echo $this->Form->control('is_gone', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+            echo $this->Form->control('title', [
+                'label' => ['floating' => true]
+            ]);
+            echo $this->Form->control('short', [
+                'label' => ['floating' => true]
+            ]);
+            echo $this->Form->control('body', [
+                'label' => ['floating' => true]
+            ]);
+            echo $this->Form->control('is_active', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('facebook_image', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+            echo $this->Form->control('is_library_item', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('facebook_image_width_override', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+            echo $this->Form->control('library_share_text', [
+                'label' => ['floating' => true]
+            ]);
+            echo $this->Form->control('is_gone', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('id_draft_parent', [
-            'type' => 'select',
-            'options' =>
-                [1 => 'Yes', 0 => 'No'],
+            echo $this->Form->control('facebook_image', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
                 'empty' => '(select one)'
             ]);
-        echo $this->Form->control('q', ['label' => 'Query']);
-        echo $this->Form->button('Filter', ['type' => 'submit']);
-        echo $this->Html->link('Reset', ['action' => 'index']);
-        echo $this->Form->end();
-    ?>
-
+            echo $this->Form->control('facebook_image_width_override', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
+                'empty' => '(select one)'
+            ]);
+            echo $this->Form->control('id_draft_parent', [
+                'type' => 'select',
+                'options' => [1 => 'Yes', 0 => 'No'],
+                'label' => ['floating' => true],
+                'empty' => '(select one)'
+            ]);
+            echo $this->Form->control('q', ['label' => 'Query']);
+            echo $this->Form->button('Filter', [
+                'type' => 'submit',
+                'class' => 'me-3'
+            ]);
+            echo $this->Html->link('Reset', ['action' => 'index']);
+            echo $this->Form->end();
+        ?>
+    </div>
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-sm">
             <thead>
