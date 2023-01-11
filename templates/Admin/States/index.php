3d7c50c8a881d3958606354dc841b154761fc7df
BrianGordon <bribios@gmail.com>
2023-01-11 12:25:34 -0800

    refs #16813 - add css and js to admin states index

--- templates/Admin/States/index.php
+++ templates/Admin/States/index.php
@@ -3,65 +3,97 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\State[]|\Cake\Collection\CollectionInterface $states
  */
+ 
+$this->Html->script('dist/admin_common.min', ['block' => true]);
 ?>
-<div class="states index content">
-    <?= $this->Html->link(__('New State'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('States') ?></h3>
-    <?= $this->element('pagination') ?>
-    <div class="table-responsive">
-        <table class="table table-striped table-bordered table-sm">
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('is_active') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($states as $state): ?>
-                <tr>
-                    <td>
-                        <?= $this->Html->link(h($state->name), [
-                            'prefix' => false,
-                            'controller' => 'locations',
-                            'action' => 'cities',
-                            'region' => $state->name,
-                            // TODO:
-                            //'region' =>  $this->Clinic->stateSlug($state->name)
-                        ]) ?>
-                    </td>
-                    <td>
-                        <?= $this->Html->badge(
-                            $state->is_active ? '<i class="bi bi-check-lg"></i> Active' : '<i class="bi bi-x-lg"></i> Inactive',
-                            [
-                                'class' => $state->is_active ? 'success' : 'danger',
-                            ]
-                        ); ?>
-                    </td>
-                    <td class="actions">
-                        <div class="btn-group-vertical btn-group-sm">
-                            <?= $this->Html->link(__('View'),
-                                [
-                                    'prefix' => false,
-                                    'controller' => 'locations',
-                                    'action' => 'cities',
-                                    'region' => $state->name,
-                                    // TODO:
-                                    //'region' =>  $this->Clinic->stateSlug($state->name)
-                                ],
-                                ['class' => 'btn btn-default']) ?>
-                            <?= $this->Html->link(__('Edit'),
-                                ['action' => 'edit', $state->id],
-                                ['class' => 'btn btn-default']) ?>
-                            <?= $this->Form->postLink(__('Delete'),
-                                ['action' => 'delete', $state->id],
-                                ['class' => 'btn btn-default', 'confirm' => __('Are you sure you want to delete # {0}?', $state->id)]) ?>
-                        </div>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <?= $this->element('pagination') ?>
+<div class="container-fluid site-body fap-cities">
+	<div class="row">
+		<div class="backdrop-container">
+			<div class="backdrop backdrop-gradient backdrop-height"></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="clear"></div>
+				<header class="col-md-12 mt10">
+					<div class="panel panel-light">
+						<div class="panel-heading">States Actions</div>
+						<div class="panel-body p10">
+							<div class="btn-group">
+								<?= $this->Html->link(__(' Browse'), ['action' => 'index'], ['class' => 'btn btn-default bi bi-search']) ?>
+								<?= $this->Html->link(__(' Add'), ['action' => 'add'], ['class' => 'btn btn-success bi bi-plus-lg']) ?>
+							</div>
+						</div>
+					</div>
+				</header>						
+				<div class="col-md-12">
+					<section class="panel">
+						<div class="panel-body">
+							<div class="panel-section expanded">
+								<h2>States</h2>
+								<div class="states index content">
+								    <?= $this->element('pagination') ?>
+								    <div class="table-responsive">
+								        <table class="table table-striped table-bordered table-sm">
+								            <thead>
+								                <tr>
+								                    <th class="p5"><?= $this->Paginator->sort('name') ?></th>
+								                    <th class="p5"><?= $this->Paginator->sort('is_active', ['label' => 'Active']) ?></th>
+								                    <th class="actions p5"><?= __('Actions') ?></th>
+								                </tr>
+								            </thead>
+								            <tbody>
+								                <?php foreach ($states as $state): ?>
+								                <tr>
+								                    <td class="p5">
+								                        <?= $this->Html->link(h($state->name), [
+								                            'prefix' => false,
+								                            'controller' => 'locations',
+								                            'action' => 'cities',
+								                            'region' => $state->name
+								                            // TODO:
+								                            //'region' =>  $this->Clinic->stateSlug($state->name)
+								                        ]) ?>
+								                    </td>
+								                    <td class="p5">
+								                        <?= $this->Html->badge(
+								                            $state->is_active ? '<i class="bi bi-check-lg"></i> Active' : '<i class="bi bi-x-lg"></i> Inactive',
+								                            [
+								                                'class' => $state->is_active ? 'success' : 'danger',
+								                            ]
+								                        ); ?>
+								                    </td>
+								                    <td class="actions p5">
+								                        <div class="btn-group-vertical btn-group-sm">
+								                            <?= $this->Html->link(__(' View'),
+								                                [
+								                                    'prefix' => false,
+								                                    'controller' => 'locations',
+								                                    'action' => 'cities',
+								                                    'region' => $state->name,
+								                                    // TODO:
+								                                    //'region' =>  $this->Clinic->stateSlug($state->name)
+								                                ],
+								                                ['class' => 'btn btn-default btn-xs bi bi-pencil-fill']) ?>
+								                            <?= $this->Html->link(__(' Edit'),
+								                                ['action' => 'edit', $state->id],
+								                                ['class' => 'btn btn-default btn-xs bi bi-eye-fill']) ?>
+								                            <?= $this->Form->postLink(__(' Delete'),
+								                                ['action' => 'delete', $state->id],
+								                                ['class' => 'btn btn-danger btn-xs bi bi-trash', 'confirm' => __('Are you sure you want to delete # {0}?', $state->id)]) ?>
+								                        </div>
+								                    </td>
+								                </tr>
+								                <?php endforeach; ?>
+								            </tbody>
+								        </table>
+								    </div>
+								    <?= $this->element('pagination') ?>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
