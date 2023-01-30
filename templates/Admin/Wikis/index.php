e6108e4074f6fdfd316a8c613e719772fac3a013
BrianGordon <bribios@gmail.com>
2023-01-30 12:41:43 -0800

    refs #16843 - add css and js to wikis index

--- templates/Admin/Wikis/index.php
+++ templates/Admin/Wikis/index.php
@@ -3,82 +3,78 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Wiki[]|\Cake\Collection\CollectionInterface $wikis
  */
+ 
+$this->Html->script('dist/admin_common.min', ['block' => true]);
 ?>
-<div class="wikis index content">
-    <?= $this->Html->link(__('New Wiki'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Wikis') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('slug') ?></th>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
-                    <th><?= $this->Paginator->sort('is_active') ?></th>
-                    <th><?= $this->Paginator->sort('id_draft_parent') ?></th>
-                    <th><?= $this->Paginator->sort('order') ?></th>
-                    <th><?= $this->Paginator->sort('title_head') ?></th>
-                    <th><?= $this->Paginator->sort('title_h1') ?></th>
-                    <th><?= $this->Paginator->sort('background_file') ?></th>
-                    <th><?= $this->Paginator->sort('meta_description') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_title') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_image') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_image_bypass') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_image_width') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_image_height') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_image_alt') ?></th>
-                    <th><?= $this->Paginator->sort('facebook_description') ?></th>
-                    <th><?= $this->Paginator->sort('last_modified') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('background_alt') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($wikis as $wiki): ?>
-                <tr>
-                    <td><?= $this->Number->format($wiki->id) ?></td>
-                    <td><?= h($wiki->name) ?></td>
-                    <td><?= h($wiki->slug) ?></td>
-                    <td><?= $this->Number->format($wiki->user_id) ?></td>
-                    <td><?= h($wiki->is_active) ?></td>
-                    <td><?= $this->Number->format($wiki->id_draft_parent) ?></td>
-                    <td><?= $this->Number->format($wiki->order) ?></td>
-                    <td><?= h($wiki->title_head) ?></td>
-                    <td><?= h($wiki->title_h1) ?></td>
-                    <td><?= h($wiki->background_file) ?></td>
-                    <td><?= h($wiki->meta_description) ?></td>
-                    <td><?= h($wiki->facebook_title) ?></td>
-                    <td><?= h($wiki->facebook_image) ?></td>
-                    <td><?= h($wiki->facebook_image_bypass) ?></td>
-                    <td><?= $this->Number->format($wiki->facebook_image_width) ?></td>
-                    <td><?= $this->Number->format($wiki->facebook_image_height) ?></td>
-                    <td><?= h($wiki->facebook_image_alt) ?></td>
-                    <td><?= h($wiki->facebook_description) ?></td>
-                    <td><?= h($wiki->last_modified) ?></td>
-                    <td><?= h($wiki->modified) ?></td>
-                    <td><?= h($wiki->created) ?></td>
-                    <td><?= h($wiki->background_alt) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $wiki->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $wiki->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $wiki->id], ['confirm' => __('Are you sure you want to delete # {0}?', $wiki->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+<div class="container-fluid site-body fap-cities">
+	<div class="row">
+		<div class="backdrop-container">
+			<div class="backdrop backdrop-gradient backdrop-height"></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="clear"></div>
+				<header class="col-md-12 mt10">
+					<div class="panel panel-light">
+						<div class="panel-heading">Help Pages Actions</div>
+						<div class="panel-body p10">
+							<div class="btn-group">
+								<?= $this->Html->link(__(' Browse'), ['action' => 'index'], ['class' => 'btn btn-default bi bi-search']) ?>
+								<?= $this->Html->link(__(' Add'), ['action' => 'edit'], ['class' => 'btn btn-success bi bi-plus-lg']) ?>
+							</div>
+						</div>
+					</div>
+				</header>						
+				<div class="col-md-12">
+					<section class="panel">
+						<div class="panel-body">
+							<div class="panel-section expanded">
+								<div class="wikis index">
+									<h2>Help Pages</h2>
+								    <?= $this->element('pagination') ?>
+								    <!-- ***TODO*** : Populate fields for advanced search --> 
+								    <?= $this->element('advanced_search') ?>
+									<div class="wikis index content">
+									    <div class="table-responsive">
+									        <table class="table table-striped table-bordered table-condensed mt20">
+									            <thead>
+									                <tr>
+										                <th><?= $this->Paginator->sort('is_active', ['label' => 'Active']) ?></th>
+									                    <th><?= $this->Paginator->sort('name') ?></th>
+									                    <th><?= $this->Paginator->sort('slug') ?></th>
+									                    <th><?= $this->Paginator->sort('short') ?></th>
+									                    <th width="120"><?= $this->Paginator->sort('last_modified', ['label' => 'Last Mod']) ?><br><?= $this->Paginator->sort('modified', ['label' => 'Last Saved']) ?></th>
+									                    <th class="actions"><?= __('Actions') ?></th>
+									                </tr>
+									            </thead>
+									            <tbody>
+									                <?php foreach ($wikis as $wiki): ?>
+									                <tr>
+										                <td><?= $wiki->is_active ? '<span class="label label-success bi bi-check-lg"> Yes</span>' : '<span class="label label-danger bi bi-x-lg"> No</span>' ?></td>
+									                    <td><?= h($wiki->name) ?></td>
+									                    <td><?= h($wiki->slug) ?></td>
+									                    <td><?= h($wiki->short) ?></td>
+									                    <td><?= date_format($wiki->last_modified, 'M j, o') ?><br><?= date_format($wiki->modified, 'M j, o') ?></td>
+									                    <td class="actions">
+										                    <div class="btn-group btn-group-vertical">
+									                        	<?= $this->Html->link(__(' View'), ['action' => 'view', $wiki->id], ['class' => 'btn btn-default btn-xs bi bi-eye-fill']) ?>
+																<?= $this->Html->link(__(' Edit'), ['action' => 'edit', $wiki->id], ['class' => 'btn btn-default btn-xs bi bi-pencil-fill']) ?>
+																<?= $this->Form->postLink(__(' Delete'), ['action' => 'delete', $wiki->id], ['confirm' => __('Are you sure you want to delete # {0}?', $wiki->id), 'class' => 'btn btn-xs btn-danger mt0 bi bi-trash']) ?>
+										                    </div>
+									                    </td>
+									                </tr>
+									                <?php endforeach; ?>
+									            </tbody>
+									        </table>
+									    </div>
+										<?= $this->element('pagination') ?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
