c45d854b315402f510d6d7f8bc501b8ff1720b99
BrianGordon <bribios@gmail.com>
2023-02-07 11:54:51 -0800

    refs #16896 - css and js on tags edit

--- templates/Admin/Tags/edit.php
+++ templates/Admin/Tags/edit.php
@@ -4,36 +4,63 @@
  * @var \App\Model\Entity\Tag $tag
  * @var string[]|\Cake\Collection\CollectionInterface $content
  */
+ 
+$this->Html->script('dist/admin_common.min', ['block' => true]);
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $tag->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $tag->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Tags'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="tags form content">
-            <?= $this->Form->create($tag) ?>
-            <fieldset>
-                <legend><?= __('Edit Tag') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('is_category');
-                    echo $this->Form->control('is_sub_category');
-                    echo $this->Form->control('header');
-                    echo $this->Form->control('display_header');
-                    echo $this->Form->control('ribbon_header');
-                    echo $this->Form->control('content._ids', ['options' => $content]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
+<div class="container-fluid site-body fap-cities">
+	<div class="row">
+		<div class="backdrop-container">
+			<div class="backdrop backdrop-gradient backdrop-height"></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="clear"></div>
+				<header class="col-md-12 mt10">
+					<div class="panel panel-light">
+						<div class="panel-heading">Tags Actions</div>
+						<div class="panel-body p10">
+							<div class="btn-group">
+								<?= $this->Html->link(__(' Browse'), ['action' => 'index'], ['class' => 'btn btn-default bi bi-search']) ?>
+								<?= $this->Html->link(__(' Add'), ['action' => 'add'], ['class' => 'btn btn-success bi bi-plus-lg']) ?>
+								<?= $this->Form->postLink(
+					                __(' Delete'),
+					                ['action' => 'delete', $tag->id],
+					                ['confirm' => __('Are you sure you want to delete # {0}?', $tag->id), 'class' => 'btn btn-danger bi bi-trash']
+					            ) ?>
+							</div>
+						</div>
+					</div>
+				</header>						
+				<div class="col-md-12">
+					<section class="panel">
+						<div class="panel-body">
+							<div class="panel-section expanded">
+						        <div class="tags form content">
+						            <?= $this->Form->create($tag) ?>
+						            <fieldset>
+						                <?php
+						                    echo $this->Form->control('name');
+						                    echo '<div class="col-md-9 col-md-offset-3 pl0 mb-3">';
+						                    echo $this->Form->control('is_category', ['required' => true]);
+						                    echo '</div>';
+						                    echo '<div class="col-md-9 col-md-offset-3 pl0 mb-3">';
+						                    echo $this->Form->control('is_sub_category', ['required' => true]);
+						                    echo '</div>';
+						                    echo $this->Form->control('header');
+						                    echo $this->Form->control('display_header', ['required' => false]);
+						                    echo $this->Form->control('ribbon_header', ['required' => false]);
+						                ?>
+						            </fieldset>
+						            <div class="form-actions tar">
+						            <?= $this->Form->button(__('Save Tag'), ['class' => 'btn btn-primary btn-lg']) ?>
+						            </div>
+						            <?= $this->Form->end() ?>
+						        </div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
