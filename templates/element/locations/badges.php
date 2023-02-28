9530243ddcb23389b0b3ed146fb4d073511136bd
BrianGordon <bribios@gmail.com>
2023-02-28 14:00:47 -0800

    refs #16899 - initial view html

---
+++ templates/element/locations/badges.php
@@ -0,0 +1,243 @@
+<?= $this->Form->control('badge_coffee', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Free Coffee',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_wifi', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Free Wifi',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_parking', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Convenient parking',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_curbside', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Curbside service',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_wheelchair', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Wheelchair accessible',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_service_pets', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Service pets welcome',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_cochlear_implants', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'style' => 'text-align:left;',
+		'text' => 'Cochlear implants and/or bone anchored hearing aids',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_ald', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Assistive listening devices',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_pediatrics', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Pediatrics',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_mobile_clinic', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Mobile clinic',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_financing', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Financing available',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_telehearing', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Telehealth services',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+</div>
+<div class="col col-md-4">
+<?= $this->Form->control('badge_asl', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'American Sign Language',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_tinnitus', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Tinnitus',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_balance', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Balance testing',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_home', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Screen/test at home',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_remote', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Remote hearing aid programming',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_mask', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Masks worn here',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_ear_cleaning', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Ear cleaning',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_spanish', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Habla Español',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_french', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'Parle Français',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_russian', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'мы говорим по-русски',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_chinese', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => '我们说中文',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
+<?= $this->Form->control('badge_punjabi', array(
+	'div' => false,
+	'label' => array(
+		'class' => 'col control-label',
+		'text' => 'ਅਸੀਂ ਪੰਜਾਬੀ ਬੋਲਦੇ ਹਾਂ',
+	),
+	'type' => 'checkbox',
+	'class' => '',
+	'wrapInput' => 'col col-md-12'
+)); ?>
