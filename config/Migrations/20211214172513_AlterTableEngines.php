5b29dece71f6a30569a89a20244172e7d17c67bc
Becky Lemons <blemons@healthyhearing.com>
2023-04-11 09:08:26 -0700

    refs #16316 - rename db columns

--- config/Migrations/20211214172513_AlterTableEngines.php
+++ config/Migrations/20211214172513_AlterTableEngines.php
@@ -14,16 +14,18 @@ class AlterTableEngines extends AbstractMigration
      */
     public function change(): void
     {
+        // Change all table engines to InnoDB
         $tables = ['advertisements', 'advertisements_clicks', 'call_sources', 'ca_call_group_notes', 'ca_call_groups', 'ca_calls', 'call_sources', 'cities', 'configurations', 'content', 'content_locations', 'content_tags', 'content_users', 'corps', 'corps_users', 'crm_searches', 'cs_calls', 'drafts', 'icing_versions', 'import_status', 'location_ads', 'location_emails', 'location_hours', 'location_notes', 'location_photos', 'location_user_logins', 'location_users', 'location_videos', 'location_vidscrips', 'locations', 'providers', 'queue_task_logs', 'queue_tasks', 'quiz_results', 'reviews', 'schema_migrations', 'seo_blacklists', 'seo_canonicals', 'seo_honeypot_visits', 'seo_meta_tags', 'seo_redirects', 'seo_search_terms', 'seo_status_codes', 'seo_titles', 'seo_uris', 'sitemap_urls', 'tag_ads', 'tag_wikis', 'tags', 'users', 'users_wikis', 'wikis', 'zipcodes'];
         foreach ($tables as $tableName) {
             try {
                 $count = $this->execute('ALTER TABLE '.$tableName.' ENGINE=InnoDB');
-                pr('Updated table engine for '.$tableName.'. '.$count.' rows updated.');
+                echo $tableName.' ';
             } catch(Exception $e) {
                 $table = $this->table($tableName, ['engine' => 'InnoDB']);
                 $table->create();
-                pr('Created table '.$tableName);
+                pr('Warning: table '.$tableName.' does not exist. Creating it.');
             }
         }
+        pr('Done');
     }
 }
---
+++ config/Migrations/20211214180000_RenameColumns.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class RenameColumns extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        try {
+            $this->table('ca_call_groups')
+                ->renameColumn('locked_by_user_id', 'id_locked_by_user')
+                ->renameColumn('xml_file_id', 'id_xml_file')
+                ->update();
+            $this->table('content')
+                ->renameColumn('brafton_id', 'id_brafton')
+                ->renameColumn('draft_parent_id', 'id_draft_parent')
+                ->update();
+            $this->table('corps')
+                ->renameColumn('order', 'priority')
+                ->renameColumn('old_id', 'id_old')
+                ->renameColumn('draft_parent_id', 'id_draft_parent')
+                ->update();
+            $this->table('crm_searches')
+                ->renameColumn('order', 'priority')
+                ->update();
+            $this->table('import_diffs')
+                ->renameColumn('model_id', 'id_model')
+                ->update();
+            $this->table('import_locations')
+                ->renameColumn('external_id', 'id_external')
+                ->renameColumn('oticon_id', 'id_oticon')
+                ->update();
+            $this->table('import_providers')
+                ->renameColumn('external_id', 'id_external')
+                ->update();
+            $this->table('location_links')
+                ->renameColumn('linked_location_id', 'id_linked_location')
+                ->update();
+            $this->table('locations')
+                ->renameColumn('oticon_id', 'id_oticon')
+                ->renameColumn('parent_id', 'id_parent')
+                ->renameColumn('sf_id', 'id_sf')
+                ->renameColumn('yhn_location_id', 'id_yhn_location')
+                ->renameColumn('cqp_practice_id', 'id_cqp_practice')
+                ->renameColumn('cqp_office_id', 'id_cqp_office')
+                ->renameColumn('coupon_id', 'id_coupon')
+                ->renameColumn('sf_id', 'id_sf')
+                ->update();
+            $this->table('providers')
+                ->renameColumn('order', 'priority')
+                ->renameColumn('yhn_location_id', 'id_yhn_location')
+                ->update();
+            $this->table('users')
+                ->renameColumn('is_active', 'active')
+                ->renameColumn('lastlogin', 'last_login')
+                ->update();
+            $this->table('wikis')
+                ->renameColumn('order', 'priority')
+                ->renameColumn('draft_parent_id', 'id_draft_parent')
+                ->update();
+        } catch(Exception $e) {
+            pr("Error: ".$e->getMessage());
+        }
+    }
+}
