5f00e32e14ea716736dd9898814c84c34c7a2fb5
Becky Lemons <blemons@healthyhearing.com>
2023-06-04 23:10:39 -0700

    test 2

--- templates/Admin/Locations/index.php
+++ templates/Admin/Locations/index.php
@@ -10,7 +10,6 @@ use Cake\Utility\Inflector;
 use Cake\Utility\Text;
 use Cake\Routing\Router;
 
-
 $siteNameAbbr = Configure::read('siteNameAbbr');
 $queryParams = $this->request->getQueryParams();
 $exportUrl = Router::url(['action' => 'export', '?' => $queryParams]);
