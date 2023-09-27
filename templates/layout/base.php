ccd3450aa9c76a410101b7d0d3acd2e9a7856f67
BrianGordon <bribios@gmail.com>
2023-09-27 12:09:42 -0700

    refs #16956 - move css and font calls further down the page

--- templates/layout/base.php
+++ templates/layout/base.php
@@ -14,11 +14,7 @@
     <!--Preload fonts-->
     <link rel="preload" href="/font/hh-icons.woff?j17ed6" as="font" type="font/woff" crossorigin>
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
-    <?= $this->Html->css('responsive'); ?>
-    <?= $this->Html->css(['BootstrapUI./font/bootstrap-icons', 'BootstrapUI./font/bootstrap-icon-sizes']); ?>
-    <?= $this->Html->script(['BootstrapUI.popper.min', 'BootstrapUI.bootstrap.min']); ?>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
@@ -30,6 +26,10 @@
     <?= $this->fetch('content') ?>
     <?= $this->fetch('bs-modals') ?>
     <?= $this->element('footer') ?>
+    <?= $this->Html->css('responsive', ['rel' => 'preload', 'as' => 'style', 'onload' => 'this.onload=null;this.rel="stylesheet"']); ?>
+    <noscript><link rel="stylesheet" href="styles.css"></noscript>
+    <?= $this->Html->css(['BootstrapUI./font/bootstrap-icons', 'BootstrapUI./font/bootstrap-icon-sizes']); ?>
+    <?= $this->Html->script(['BootstrapUI.popper.min', 'BootstrapUI.bootstrap.min']); ?>
     <!--/*** TODO: uncomment this once GTM is pulled in: ?= $this->element('cookie_footer')*/ ?-->
 </body>
 <?= $this->fetch('script') ?>
