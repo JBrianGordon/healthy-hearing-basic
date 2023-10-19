a23fd1954a548e6ed6ad0eaf2099ed8055ea737e
BrianGordon <bribios@gmail.com>
2023-10-19 14:14:58 -0700

    refs #17032 - check controllers to determine footer type

--- templates/element/sticky_footer.php
+++ templates/element/sticky_footer.php
@@ -1,18 +1,20 @@
 <?php
 	use Cake\Core\Configure;
+	use Cake\ORM\TableRegistry;
+	use App\Model\Entity\Location;
 ?>
 <a id="backToTop" class="row noprint quickLink back-to-top-link btn btn-light btn-sm" href="#top">
 	<p>Back to top</p>
 </a>
-<footer id="stickyFooter" class="col-xs-12 w-100 sticky-footer noprint">
+<footer id="stickyFooter" class="d<?= isset($location->id) || $this->getRequest()->getParam('controller') == 'Locations' || $this->getRequest()->getParam('controller') == 'Content' || $this->getRequest()->getParam('controller') == 'Wikis' || $this->getRequest()->getParam('controller') == 'Corps' ? "-lg" : ""?>-none col-xs-12 w-100 sticky-footer noprint">
 	<div class="col-xs-12 w-100 mt10 mb10">
-		<?php if ($this->layout == 'profile'): ?>
+		<?php if (isset($location->id)): ?>
 			<?php 
 				$isEnhancedOrPremier = $this->Clinic->isEnhancedOrPremierByLocationId($location->id);
 				$displayOpenClosed = $this->Clinic->getOpenClosedByLocationId($location->id);
 			?>
 			<!-- Clinic profile pages -->
-			<?php if (Configure::read('isCallAssistEnabled') && $location->is_call_assist && $isEnhancedOrPremier && !ClassRegistry::init('Configuration')->isCallTrackingBypassed()): ?>
+			<?php if (Configure::read('isCallAssistEnabled') && $location->is_call_assist && $isEnhancedOrPremier && !TableRegistry::get('Configurations')->isCallTrackingBypassed()): ?>
 				<div>
 					<?php if (in_array($location->direct_book_type, [Location::DIRECT_BOOK_BLUEPRINT, Location::DIRECT_BOOK_EARQ]) && !empty($location->direct_book_iframe)): ?>
 						<a href="#" class="btn btn-secondary directBookBtn" data-button="<?= $location->id; ?>" style="min-width:180px;">Book now!</a>
@@ -22,15 +24,16 @@
 				</div>
 				<?php if(!empty($displayOpenClosed) || !Configure::read('isCallAssistEnabled')): ?>
 					<div class="telephone mt5">
-						Or call: <strong><?= $this->Clinic->phone($location, ['link' => $is_mobile]); ?></strong>
+						Or call: <strong><?= $this->Clinic->phone($location); ?></strong>
 					</div>
 				<?php endif; ?>
 			<?php else: ?>
 				<div class="telephone">
-					<span class="glyphicon glyphicon-earphone"></span> <strong><?= $this->Clinic->phone($location, ['link' => $is_mobile]); ?></strong>
+					<span class="glyphicon glyphicon-earphone"></span> <strong><?= $this->Clinic->phone($location); ?></strong>
 				</div>
 			<?php endif; ?>
 		<?php elseif ($this->getRequest()->getParam('controller') == 'Locations'): ?>
+		<!--*** TODO: set up quick pick ***-->
 			<?php if (isFeatureOn('quick_pick')): ?>
 				<p class="message-800-number" class="text-large">
 					<span class="telephone">Call <a href=<?= "tel:+".Configure::read('quickPickNumber'); ?>><?= Configure::read('quickPickNumber'); ?></a></span> to book a test<br>with a clinic near you.
@@ -39,7 +42,7 @@
 				<!-- Nothing to display. Hide the sticky footer. -->
 				<script>document.getElementById("stickyFooter").style.display = "none";</script>
 			<?php endif; ?>
-		<?php else: ?>
+		<?php elseif($this->getRequest()->getParam('controller') == 'Content' || $this->getRequest()->getParam('controller') == 'Wikis' || $this->getRequest()->getParam('controller') == 'Corps'): ?>
 			<!-- Report/Help/Manufacturer pages -->
 			<div>
 				<a href="<?= $this->Clinic->nearMeLink();?>" class="btn btn-secondary">Connect with clinics near me</a>
