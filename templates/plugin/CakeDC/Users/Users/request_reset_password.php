cb8f9c128c31d11e3d285358e0e18e5195a6b9f6
BrianGordon <bribios@gmail.com>
2023-11-07 08:46:47 -0800

    refs #16875 - forgot password styles

--- templates/plugin/CakeDC/Users/Users/request_reset_password.php
+++ templates/plugin/CakeDC/Users/Users/request_reset_password.php
@@ -2,14 +2,38 @@
 /**
  * @var \CakeDC\Users\Model\Entity\User $user
  */
+
+use Cake\Core\Configure;
 ?>
-<div class="users form">
-    <?= $this->Flash->render('auth') ?>
-    <?= $this->Form->create($user) ?>
-    <fieldset>
-        <legend><?= __d('cake_d_c/users', 'Please enter your email or username to reset your password.') ?></legend>
-        <?= $this->Form->control('reference') ?>
-    </fieldset>
-    <?= $this->Form->button(__d('cake_d_c/users', 'Submit')); ?>
-    <?= $this->Form->end() ?>
+<div class="container-fluid site-body fap-cities">
+    <div class="row">
+        <div class="backdrop-container">
+            <div class="backdrop backdrop-gradient backdrop-height"></div>
+        </div>
+        <div class="container">
+            <div class="row pt20">
+                <div class="col-sm-12 panel-parent">
+                    <section class="panel">
+                        <div class="panel-body">
+                            <div class="panel-section expanded">
+                                <div class="users form">
+                                    <h1>Forgot Password</h1>
+                                    <?= $this->Flash->render('auth') ?>
+                                    <?= $this->Form->create($user) ?>
+                                    <fieldset>
+                                        <?= $this->Form->control('reference', ['label' => 'Account number', 'required' => true]) ?>
+                                        <small>Having trouble logging in?  Please contact our Customer Support at <?= Configure::read('phone') ?> or <a href="mailto:<?= Configure::read('customer-support-email') ?>"><?= Configure::read('customer-support-email') ?></a>.</small>
+                                    </fieldset>
+                                    <div class="form-group pt20">
+                                        <?= $this->Form->button(__d('cake_d_c/users', 'Reset password'), ['class' => 'btn btn-primary btn-lg']); ?>
+                                    </div>
+                                    <?= $this->Form->end() ?>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
