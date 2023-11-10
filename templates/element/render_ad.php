fd56cc7eea49c49a4ccf7eed35761b3852fa30ef
BrianGordon <bribios@gmail.com>
2023-11-10 13:29:15 -0800

    refs #16833 - newsletter page css and js

--- templates/element/render_ad.php
+++ templates/element/render_ad.php
@@ -22,7 +22,7 @@ if (!empty($ad->id) && $show_ad) {
 	// make passthrough - desc & src
 	$dest = '/ads/click/'.$id;
 	// Order
-	if((!empty($wiki) || !empty($content) || !empty($corp) || !empty($stateNice) || !empty($corps)) && !$isMobileDevice){
+	if((!empty($wiki) || !empty($content) || !empty($corp) || !empty($stateNice) || !empty($corps)  || !empty($page)) && !$isMobileDevice){
 		$adOrder = '7';
 	} else if ((!empty($wiki) || !empty($content) || !empty($corp) || !empty($stateNice) || !empty($corps)) && $isMobileDevice){
 		$adOrder = '1';
