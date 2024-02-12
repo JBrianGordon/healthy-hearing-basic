37b16222130aa8bb3cbaaf5c7ee305bb2473d96e
BrianGordon <bribios@gmail.com>
2024-02-12 15:06:27 -0800

    refs #17100 - seo meta tag templates and controller

---
+++ templates/Admin/SeoMetaTags/edit.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\SeoMetaTag $seoMetaTag
+ * @var string[]|\Cake\Collection\CollectionInterface $seoUris
+ */
+
+$this->Html->script('dist/admin_common.min', ['block' => true]);
+?>
+<header class="col-md-12 mt10">
+    <div class="panel panel-light">
+        <div class="panel-heading">Meta Tags Actions</div>
+        <div class="panel-body p10">
+            <div class="btn-group">
+                <?= $this->Html->link(' Browse', ['action' => 'index'], ['class' => 'btn btn-default bi bi-search']) ?>
+                <?= $this->Html->link(' Add', ['action' => 'add'], ['class' => 'btn btn-success bi bi-plus-lg']) ?>
+                <?= $this->Html->link(' Delete', ['action' => 'delete', $seoMetaTag->id], ['confirm' => __('Are you sure you want to delete # {0}?', $seoMetaTag->id), 'class' => 'btn btn-danger bi-trash-fill', 'id' => 'deleteBtn']) ?>
+                <?= $this->Html->link(' Blacklists', ['controller' => 'seoBlacklists', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+                <?= $this->Html->link(' Canonical', ['controller' => 'seoCanonicals', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+                <?= $this->Html->link(' Redirects', ['controller' => 'seoRedirects', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+                <?= $this->Html->link(' Status codes', ['controller' => 'seoStatusCodes', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+                <?= $this->Html->link(' Titles', ['controller' => 'seoTitles', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+                <?= $this->Html->link(' Uris', ['controller' => 'seoUris', 'action' => 'index'], ['class' => 'btn btn-default']) ?>
+            </div>
+        </div>
+    </div>
+</header>                       
+<div class="col-md-12">
+    <section class="panel">
+        <div class="panel-body">
+            <div class="panel-section expanded">
+                <h2>Edit SEO Meta Tag</h2>
+                <hr>
+                <div class="row">
+                    <div class="column-responsive column-80">
+                        <div class="seoMetaTags form content">
+                            <?= $this->Form->create($seoMetaTag) ?>
+                            <fieldset>
+                                <?php
+                                    /*** TODO: replace seo_uri_id with the uri value (e.g. /beltone-hearing-aids) ***/
+                                    echo $this->Form->control('seo_uri_id', ['type' => 'text', 'label' => 'Uri', 'required' => true]);
+                                    echo $this->Form->control('name', ['required' => true]);
+                                    echo $this->Form->control('content', ['required' => true]);
+                                ?>
+                                <div class="col-md-9 col-md-offset-3 pl0">
+                                    <?= $this->Form->control('is_http_equiv', ['required' => true]) ?>
+                                </div>
+                            </fieldset>
+                            <div class="form-actions tar">
+                                <?= $this->Form->button('Save Meta Tag', ['class' => 'btn btn-primary btn-lg']) ?>
+                            </div>
+                            <?= $this->Form->end() ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
