dfbf70333819a03ed6eb95ec0e8830332348c71f
Bill Talkington <btalkington@healthyhearing.com>
2024-07-01 14:37:46 -0400

    Refs #17146 - add Deployer to Cake4 project

---
+++ deploy.example.php
@@ -0,0 +1,117 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+//*******************\\
+//***** Config ******\\
+//-------------------\\
+
+// Dev site
+$devUrl = 'DEV_URL';
+$devAlias = 'DEV_ALIAS';
+$devDeployPath = '/DEV/DEPLOY/PATH';
+
+// CA-Dev site
+$caDevUrl = 'CA_DEV_URL';
+$caDevAlias = 'CA_DEV_ALIAS';
+$caDevDeployPath = '/CA/DEV/DEPLOY/PATH';
+
+// SSH settings
+$remoteUser = 'REMOTE_USER';
+$identityFile = '~/.ssh/IDENTITY_FILE';
+$sshControlPath = '/SSH/CONTROL/PATH/';
+
+set('repository', 'git@github.com:GIT_ORG/GIT_REPO.git');
+set('keep_releases', 3);
+
+//*******************\\
+//****** Hosts ******\\
+//-------------------\\
+
+host($devUrl)
+    ->set('remote_user', $remoteUser)
+    ->set('identity_file', $identityFile)
+    ->set('ssh_control_path', $sshControlPath . '%r@%h:%p')
+    ->set('deploy_path', $devDeployPath)
+    ->setLabels([
+        'system' => $devAlias
+    ]);
+
+host('ca-depl.hhcake.com')
+    ->set('remote_user', $remoteUser)
+    ->set('identity_file', $identityFile)
+    ->set('ssh_control_path', $sshControlPath . '%r@%h:%p')
+    ->set('deploy_path', $caDevDeployPath)
+    ->setLabels([
+        'system' => $caDevAlias
+    ]);
+
+
+//*******************\\
+//****** Tasks ******\\
+//-------------------\\
+
+task('build', function () {
+    cd('{{release_path}}');
+    run('bin/cake bootstrap install --latest');
+    run('npm install');
+    run('npm run build');
+    run('npm run compile-sass');
+});
+
+task('deployDevSites', function() {
+    runLocally('dep deploy system=' . $devAlias . ' && dep deploy system=' . $caDevAlias);
+});
+
+
+//*******************\\
+//****** Hooks ******\\
+//-------------------\\
+
+after('deploy:vendors', 'build');
+after('deploy:failed', 'deploy:unlock');
+
+
+//************************************************\\
+//*** CakePHP 4 Project Template configuration ***\\
+//------------------------------------------------\\
+
+// CakePHP 4 Project Template shared dirs
+set('shared_dirs', [
+    'logs',
+    'tmp',
+]);
+
+// CakePHP 4 Project Template shared files
+set('shared_files', [
+    'config/.env',
+    'config/app.php',
+    'config/app_local.php'
+]);
+
+/**
+ * Create plugins' symlinks // ------ SEE NOTE BELOW IN 'deploy' TASK
+ */
+task('deploy:init', function () {
+    run('{{bin/php}} {{release_or_current_path}}/bin/cake.php plugin assets symlink');
+})->desc('Initialization');
+
+/**
+ * Run migrations
+ */
+task('deploy:run_migrations', function () {
+    run('{{bin/php}} {{release_or_current_path}}/bin/cake.php migrations migrate --no-lock');
+    run('{{bin/php}} {{release_or_current_path}}/bin/cake.php schema_cache build');
+})->desc('Run migrations');
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    // 'deploy:init',// temporarily? unused,symlink already in repo causing issue
+    'deploy:run_migrations',
+    'deploy:publish',
+])->desc('Deploy your project');
