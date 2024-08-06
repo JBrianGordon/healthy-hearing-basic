107fa87a3b923243a7a6f4c130f6ffe0e47b6bd7
BrianGordon <bribios@gmail.com>
2024-08-06 09:27:45 -0700

    refs #17169 - closing script fix

--- templates/element/google_tag_manager.php
+++ templates/element/google_tag_manager.php
@@ -16,7 +16,7 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','<?= Configure::read('gtmId'); ?>')</script>
+})(window,document,'script','dataLayer','<?= Configure::read('gtmId'); ?>');
 
 //List of data pushes pushed in this request
 dataLayer.dataPushes = [];
