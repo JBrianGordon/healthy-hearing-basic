124e576f20ac39dd340c4ffdcee8444a468ff44b
BrianGordon <bribios@gmail.com>
2024-09-19 16:07:08 -0700

    refs #16824 - remove reviewForm and update template variables

--- templates/email/html/Review/positiveReviewReceived.php
+++ templates/email/html/Review/positiveReviewReceived.php
@@ -12,12 +12,12 @@ use Cake\Routing\Router;
 
 <p>Congratulations!</p>
 
-<p>The <?= $siteName ?> profile for your clinic has just received a new review. You can view the profile and see the review here:</p>
+<p>The <?= Configure::read('siteName') ?> profile for your clinic has just received a new review. You can view the profile and see the review here:</p>
 
 <p><?= $this->Html->link(Router::url($url, true)) ?></p>
 
 <?php if ($isUS): ?>
-	<p>Reviews are an effective way to build your clinic’s online reputation and help this profile stand out amongst the competition; <?= $siteName ?> profiles with reviews get three times more calls and appointments than those without reviews. Don’t be shy about requesting reviews from your patients.</p>
+	<p>Reviews are an effective way to build your clinic’s online reputation and help this profile stand out amongst the competition; <?= Configure::read('siteName') ?> profiles with reviews get three times more calls and appointments than those without reviews. Don’t be shy about requesting reviews from your patients.</p>
 	<p>You can respond to reviews by logging into your Healthy Hearing profile and clicking the Reviews button.</p>
 <?php else: ?>
 	<p>Reviews are an effective way to build your clinic’s online reputation and help this profile stand out amongst the competition so don’t be shy about requesting reviews from your patients.</p>
