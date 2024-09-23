2184cdcd6bd47fc9c679f5d2768105f23d454b58
Bill Talkington <btalkington@healthyhearing.com>
2024-09-23 12:07:49 -0400

    Refs #17051 - fix sieName typo, delete canada-specific negative email template

--- templates/email/html/Review/review_response_posted.php
+++ templates/email/html/Review/review_response_posted.php
@@ -13,7 +13,7 @@ Thank you for submitting a response to a review of your clinic <?= $clinicTitle 
 
 When a clinic receives a review that is less than positive, a sincere reply from the clinic can help build a positive reputation. The original reviewer is not notified if and when you submit a response. However, responding to a review is a great way to demonstrate to future profile visitors that your clinic team takes customer satisfaction seriously.<br /><br />
 
-Reviews are an effective way to build your clinic’s online reputation and help this profile stand out amongst the competition; <?= Configure::read('sieName') ?> profiles with reviews get three times more calls and appointments than those without reviews. Don’t be shy about suggesting to all of your patients that they review your practice.<br /><br />
+Reviews are an effective way to build your clinic’s online reputation and help this profile stand out amongst the competition; <?= Configure::read('siteName') ?> profiles with reviews get three times more calls and appointments than those without reviews. Don’t be shy about suggesting to all of your patients that they review your practice.<br /><br />
 
 If you have any questions about reviews or would like suggestions about how to obtain more, please contact us at <?= $this->Html->link(Configure::read('customer-support-email'), 'mailto:' . Configure::read('customer-support-email')) ?>.<br /><br />
 
