315b31dff6c174d83c873962b1cec036fb3b51ca
BrianGordon <bribios@gmail.com>
2024-10-04 10:38:47 -0700

    refs #17156 - add outbound template and layout

--- templates/element/ca_calls/note.php
+++ templates/element/ca_calls/note.php
@@ -4,18 +4,18 @@ use App\Model\Entity\CaCallGroup;
 $body = strip_tags($note->body, '<div><ul><li><a><span><br>');
 $status = isset(CaCallGroup::$statuses[$note->status]) ? CaCallGroup::$statuses[$note->status] : '';
 ?>
-<div class="single_note">
-	<table cellpadding="0" cellspacing="0">
+<div class="single_note m10">
+	<table cellpadding="0" cellspacing="0" class="border-0 mb0">
 		<tr>
-			<td class="note_who"><?php echo $this->Clinic->getUserName($note->user_id); ?></td>
-			<td class="status"><?php echo $status; ?></td>
-			<td class="when"><?php echo dateTimeCentralToEastern($note->created); ?></td>
-			<td class="delete">
-				<?php echo $this->Html->link('Delete', ['controller' => 'ca_call_group_notes', 'action' => 'delete', $note->id], ['class' => 'btn btn-danger btn-xs'], 'Are you sure?'); ?>
+			<td class="note_who border-0 p5" style="min-width:180px"><?= $this->App->getUserName($note->user_id) ?></td>
+			<td class="status border-0 p5" style="min-width:200px"><?= $status ?></td>
+			<td class="when border-0 p5" style="min-width:200px"><?= dateTimeCentralToEastern($note->created) ?></td>
+			<td class="delete border-0 p5">
+				<?= $this->Html->link('Delete', ['controller' => 'ca_call_group_notes', 'action' => 'delete', $note->id], ['class' => 'btn btn-danger btn-xs'], 'Are you sure?') ?>
 			</td>
 		</tr>
 		<tr>
-			<td colspan="3" class="body"><?php echo $body; ?></td>
+			<td colspan="3" class="body border-0 p5"><?= $body ?></td>
 		</tr>
 	</table>
 </div>
