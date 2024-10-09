0750e183866616bb0a602fd75136f43d82896d4b
BrianGordon <bribios@gmail.com>
2024-10-09 13:27:15 -0700

    refs #16824 - QuizResults Mailer and action

--- src/Mailer/QuizResultsMailer.php
+++ src/Mailer/QuizResultsMailer.php
@@ -2,33 +2,29 @@
 namespace App\Mailer;
 
 use Cake\Mailer\Mailer;
-use function debug;
 
 class QuizResultsMailer extends Mailer
 {
-    public function sendQuizResult($data)
+    public static $name = 'QuizResults';
+
+    public function sendQuizResult($name, $hearingResult, $toEmail, $symptoms)
     {
-        $results = json_decode($data, true);
-        $name = $results['firstName'] . ' ' . $results['lastName'];
-        $hearingResult = $results['hearingResult'];
-        $toEmail = $results['email'];
-
-        $template = 'quizResultNormal';
+        $template = 'quiz_result_normal';
         if ($hearingResult == 'possible') {
-            $template = 'quizResultPossible';
+            $template = 'quiz_result_possible';
         } else if ($hearingResult == 'significant') {
-            $template = 'quizResultSignificant';
+            $template = 'quiz_result_significant';
         }
-
         $this
             ->setEmailFormat('html')
             ->setTo($toEmail)
             ->setSubject('Quiz Result')
-            ->setTemplate("QuizResults/$template")
             ->setViewVars([
-                'results' => $results,
+                'symptoms' => $symptoms,
                 'name' => $name
-            ]);
+            ])
+            ->viewBuilder()
+                ->setTemplate("QuizResults/$template");
 
         return $this;
     }
