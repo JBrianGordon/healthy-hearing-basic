818352c3769204242681032ffa76d563f5c3fa1e
BrianGordon <bribios@gmail.com>
2025-01-29 11:06:03 -0800

    refs #17120 - modify fb and pinterest share buttons

--- templates/element/content/share.php
+++ templates/element/content/share.php
@@ -11,46 +11,26 @@ if (isset($wiki)) {
 	$description = '';
 	$image = '/img/hh-symbol.png';
 }
+
+$currentUrl = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$encodedUrl = urlencode($currentUrl);
 $image = Router::url($image, true);
 ?>
 <footer class="panel-footer text-center btn-set noprint">
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0" nonce="8cB2VAXE"></script>
-	<div id="fbBtn" class="btn btn-share btn-facebook ml0">
-		<span class="hh-icon-facebook"></span>Share
+	<div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v22.0"></script>
+	<div id="fbBtn" class="btn btn-share btn-facebook fb-share-btn ml0" data-href="https://developers.facebook.com/docs/plugins/" data-layout="" data-size="">
+		<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?= $encodedUrl ?>&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">
+			<span class="hh-icon-facebook"></span> Share
+		</a>
 	</div>
-	<script>
-	document.getElementById('fbBtn').onclick = function(e) {
-	  e.preventDefault();
-	  FB.ui({
-	    display: 'popup',
-	    method: 'share',
-	    href: window.location.href,
-	  }, function(response){});
-	}
-	</script>
-	<a href="https://x.com/intent/tweet?url=<?= urlencode($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) ?>" class="btn btn-share btn-twitter twitter-share-button ml15" id="twitter-wjs" target="_blank" rel="noopener"><span class="hh-icon-x"></span> Tweet</a>
+	<a href="https://x.com/intent/tweet?url=<?= $encodedUrl ?>" class="btn btn-share btn-twitter twitter-share-button ml15" id="twitter-wjs" target="_blank" rel="noopener"><span class="hh-icon-x"></span> Tweet</a>
 	<!-- LinkedIN JS -->
 	<script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
-	<script type="IN/Share" data-url="https://<?= htmlspecialchars($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8') ?>"></script>
-	<?php
-	if(empty($hidePinterest)) {
-		echo $this->Share->pinterest([
-			'linkOptions' => [
-				'class' => 'btn btn-share btn-pinterest ml15',
-				'target' => '_blank',
-				'rel' => 'noopener',
-				'escape' => false,
-				'onclick' => false,
-				'data-pin-do' => 'buttonPin',
-				'media' => $image,
-				'href' => '#'
-			],
-			'label' => '<span class="hh-icon-pinterest"></span> Pin',
-			'text' => $description,
-			'image' => $image
-		]);
-	}
-	?>
+	<script type="IN/Share" data-url="<?= $encodedUrl ?>"></script>
+	<a href="https://www.pinterest.com/pin/create/button/?media=<?= $image ?>&amp;url=<?= $encodedUrl ?>&amp;description=<?= $description ?>" class="btn btn-share btn-pinterest" target="_blank" rel="noopener">
+		<span class="hh-icon-pinterest"></span> Pin
+	</a>
 	<button class="btn btn-print btn-light noprint ml15" onclick="window.print()"><span class="icon hh-icon-printer"></span> Print</button>
 	<!-- Pinterest button JS -->
 	<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js" data-pin-custom="true"></script>
