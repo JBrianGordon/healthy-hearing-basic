341b95207c0be6434d2b0907485a61f7edef7ce5
Bill Talkington <btalkington@healthyhearing.com>
2025-03-13 10:36:45 -0400

    Refs 17077 - use logo_url on corps index page

--- templates/Corps/index.php
+++ templates/Corps/index.php
@@ -33,7 +33,7 @@ $this->Breadcrumbs->add([
 					          <div class="row">
 					            <div class="col-xl-3 gutter-below">
 						            <div class="logo-container">
-					            		<img src="<?= $corp->thumb_url ?>" loading="lazy" alt="<?= $corp->title ?>" class="img-responsive align-center" width="150px">
+													<img src="<?= $corp->logo_url ?>" loading="lazy" alt="<?= $corp->title ?>" class="img-responsive align-center" width="150px">
 						            </div>
 					            </div>
 					            <div class="col-xl-9">
