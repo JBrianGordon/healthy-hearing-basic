ea8727034dc5abe0bd1f5fc056e6b87687ffdb12
BrianGordon <bribios@gmail.com>
2025-10-30 14:18:04 -0700

    refs #17270 - comment out c/z details

--- templates/Locations/view_city_zip.php
+++ templates/Locations/view_city_zip.php
@@ -36,10 +36,10 @@ $this->Html->script('dist/location_results.min.js?v='.Configure::read("tagVersio
 						<div class="panel-section expanded">
 							<header class="row">
 								<div class="col-md-12">
-									<h1 class="text-primary">Hearing <?= Configure::read('regionalSpelling.center')?>s <?= (!empty($is_near)) ? 'near' : 'in' ?> <?= $this->Clinic->nearText($region,$city,$zip) ?></h1>
+									<h1 class="text-primary" style="margin-bottom:0">Hearing <?= Configure::read('regionalSpelling.center')?>s <?= (!empty($is_near)) ? 'near' : 'in' ?> <?= $this->Clinic->nearText($region,$city,$zip) ?></h1>
 								</div>
 							</header>
-							<p class="lead text-primary mb20" id="filter-results">
+							<p class="lead text-primary mb0" id="filter-results">
 								<?php if($state == "Quebec") : ?>
 								<p>If you are looking for a clinic in Quebec, please visit the <a href="https://www.ordreaudio.qc.ca/chercher-un-audioprothesiste/" rel="noopener" target="_blank">Ordre des Audioprothésistes du Québec</a> website.</p>
 								<?php elseif (empty($locations)): ?>
@@ -51,24 +51,25 @@ $this->Html->script('dist/location_results.min.js?v='.Configure::read("tagVersio
 										<?= $this->element('locations/search', ['label' => "Search by city or ".$zipShort]) ?>
 									</div>
 								<?php else: ?>
-									<i>
-										<?php
+									<!-- Commented out in case we do ever decide to bring this information back. -->
+									<!--<i>
+										<?php /*
 										$clinicCount = count($locations);
 										$s = ($clinicCount == 1) ? '' : 's';
 										echo 'We found ' . $clinicCount . ' hearing aid clinic' . $s;
 										if ($reviewCount) {
 											$s = ($reviewCount == 1) ? '' : 's';
 											echo ' with ' . $reviewCount . ' review' . $s;
-										}
+										} */
 										?>
-										within <?= $this->Clinic->highestDistance($locations) ?> of <?= $this->Clinic->nearText($region,$city,$zip) ?>.
-										<?php $nearMeLink = $this->Clinic->nearMeLink(); ?>
-										<?php if (stripos($_SERVER['REQUEST_URI'], $nearMeLink) === false): ?>
-											Show clinics <a href="<?= $nearMeLink ?>" class="text-secondary">near me</a>.
-										<?php endif; ?>
+										within <?php /* echo $this->Clinic->highestDistance($locations) */ ?> of <?php /* echo $this->Clinic->nearText($region,$city,$zip) */ ?>.
+										<?php /* $nearMeLink = $this->Clinic->nearMeLink(); */ ?>
+										<?php /* if (stripos($_SERVER['REQUEST_URI'], $nearMeLink) === false): */ ?>
+											Show clinics <a href="<?php /* echo $nearMeLink */ ?>" class="text-secondary">near me</a>.
+										<?php /* endif; */ ?>
 									</i>
 									<br>
-									<p class="mb15"><?= $siteName ?> provides listings from audiologists, hearing instrument specialists and hearing aid <?= Configure::read('regionalSpelling.center') ?>s near you. If you need hearing aids or a hearing test, choose a clinic from the list below to schedule an appointment in your area.</p>
+									<p class="mb15"><?php /* echo $siteName */ ?> provides listings from audiologists, hearing instrument specialists and hearing aid <?php /* echo Configure::read('regionalSpelling.center') */ ?>s near you. If you need hearing aids or a hearing test, choose a clinic from the list below to schedule an appointment in your area.</p>-->
 									<?php if ($isAdmin): ?>
 										<?php
 										$exportLink = "/admin/locations/export";
