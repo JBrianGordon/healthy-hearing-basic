0ef88bb7e232205b27c44ea0a4ab58f0121b1aef
Bill Talkington <btalkington@healthyhearing.com>
2025-12-18 11:05:44 -0500

    Refs #16880 - updates to crons.php

--- config/crons.php
+++ config/crons.php
@@ -16,12 +16,12 @@
 // -- DAILY PROCESSES
 // ----------------------------------------------------------------------------------------
 // ----- | (DAILY - Early morning)
-// ------- | 30 5 * * * /var/www/prod/hh/app/scripts/daily_shells.sh
+// ------- | 30 5 * * * /var/www/prod/hh/app/scripts/daily_shells.sh (RUNNING ON HHAPP4)
 // -----------------------------------------------------------------
 // ./cake call_sources importLeadscoreReports
 // ./cake ca_calls exportDailyYhnCallData
-// ./cake ca_calls clearOldOutboundCalls
-// ./cake ca_calls importBlueprintAppts
+// PAUSED - In ticket #15221 -- ./cake ca_calls clearOldOutboundCalls
+// ./cake ca_calls importBlueprintAppts (RUNNING ON HHAPP4)
 // ./cake reviews clear_spam
 // ./cake locations endGracePeriods
 // ./cake locations unfreezeListingTypes
@@ -55,10 +55,8 @@
 30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_Corp && curl -s https://dev.hhcake.com/sitemap_Corp.xml > /dev/null
 30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_Help && curl -s https://dev.hhcake.com/sitemap_Help.xml > /dev/null
 30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_State && curl -s https://dev.hhcake.com/sitemap_State.xml > /dev/null
-// ./cake locations updateMetrics
-// REMOVED -- COUNTMETRICS WERE REMOVED IN https://redmine.healthyhearing.com/issues/17166
-// ./cake locations updateMetricsCache
-// REMOVED -- COUNTMETRICS WERE REMOVED IN https://redmine.healthyhearing.com/issues/17166
+// REMOVED -- COUNTMETRICS REMOVED IN #17166 - // ./cake locations updateMetrics
+// REMOVED -- COUNTMETRICS REMOVED IN #17166 - // ./cake locations updateMetricsCache
 // ?? -- GETTING RID OF THIS? -- // ./cake util cache_permissions
 // ?? -- HOW WILL WE REPORT ERRORS? -- // ./cake mail sendCronEmail "Nightly" $FILENAME
 
@@ -107,17 +105,55 @@
 * * * * *  cd /var/www/prod/current && bin/cake queue run -q
 
 // -- DAILY PROCESSES
+// ----------------------------------------------------------------------------------------
 // ----- | (DAILY - Early morning)
-
-// 50 5 * * * /var/www/prod18/hh/app/scripts/daily_shells.sh
+// ------- | 50 5 * * * /var/www/prod18/hh/app/scripts/daily_shells.sh
+// -----------------------------------------------------------------
+// ./cake call_sources importLeadscoreReports
+// SKIPPED ON CA/HD - CALL ASSIST DISABLED - ./cake ca_calls exportDailyYhnCallData
+// PAUSED - In ticket #15221 -- ./cake ca_calls clearOldOutboundCalls
+// HH-ONLY ON HHAPP4 -./cake ca_calls importBlueprintAppts
+// ./cake reviews clear_spam
+// SKIPPED ON CA/HD - TIERING IS DISABLED - ./cake locations endGracePeriods
+// SKIPPED ON CA/HD - TIERING IS DISABLED - ./cake locations unfreezeListingTypes
+// SKIPPED ON CA/HD - TIERING IS DISABLED - ./cake locations findExpiredFeatures
+// HH-ONLY ON HHAPP4 - ./cake locations findListingTypesForOticon
+// HH-ONLY ON HHAPP4 - ./cake locations findListingTypesForCqp
+// ./cake locations updateReviewCounts
+// SKIPPED ON CA/HD - REPORTS ARE DISABLED - ./cake content freezeContent
+// ?? -- GETTING RID OF THIS? -- ./cake util cache_permissions
+// ?? -- HOW WILL WE REPORT ERRORS? -- ./cake mail sendCronEmail "Daily" $FILENAME
 
 // ----- | (DAILY - Midnight/Night)
-
-// 30 0 * * * /var/www/prod18/hh/app/scripts/midnight_shells.sh
+// ------- | 30 0 * * * /var/www/prod18/hh/app/scripts/midnight_shells.sh
+// ----------------------------------------------------------------------
+// SKIPPED ON CA/HD - REPORTS ARE DISABLED - ./cake content set_last_modified
+// SKIPPED ON CA/HD - REPORTS ARE DISABLED - ./cake content publishContent
+// ./cake wiki publishWiki
+30 5 * * * cd /var/www/prod/current && bin/cake editorial publish Wikis
+// SKIPPED ON CA/HD - NO CORP PAGES - ./cake corp publishCorp
+// ./scripts/regenerate_sitemap.sh
+# Regenerate query caches for sitemaps
+30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_City && curl -s https://dev.hhcake.com/sitemap_City.xml > /dev/null
+30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_Clinic && curl -s https://dev.hhcake.com/sitemap_Clinic.xml > /dev/null
+30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_Help && curl -s https://dev.hhcake.com/sitemap_Help.xml > /dev/null
+30 0 * * * cd /var/www/prod/shared/tmp/cache/ && rm -f cake_sitemap_query_State && curl -s https://dev.hhcake.com/sitemap_State.xml > /dev/null
+// REMOVED -- COUNTMETRICS REMOVED IN #17166 - // ./cake locations updateMetrics
+// REMOVED -- COUNTMETRICS REMOVED IN #17166 - // ./cake locations updateMetricsCache
+// ?? -- GETTING RID OF THIS? -- // ./cake util cache_permissions
+// ?? -- HOW WILL WE REPORT ERRORS? -- // ./cake mail sendCronEmail "Nightly" $FILENAME
 
 // -- WEEKLY PROCESSES
-
-// 30 6 * * 0 /var/www/prod18/hh/app/scripts/weekly_shells.sh
+// ----------------------------------------------------------------------------------------
+// ------- | 30 6 * * 0 /var/www/prod18/hh/app/scripts/weekly_shells.sh
+// --------------------------------------------------------------------
+// ./cake city addAndUpdateCities
+// ./cake city addAndUpdateCitiesByRange
+// ./cake city findInvalidCities
+// ./cake locations findDuplicates 1
+// ./cake locations cleanupProviders
+// ?? -- GETTING RID OF THIS? -- ./cake util cache_permissions
+// ?? -- HOW WILL WE REPORT ERRORS? -- ./cake mail sendCronEmail "Weekly" $FILENAME
 
 /*
 |--------------------------------------------------------------------------
